5b61b08104a95f1c3effaae384543a0a212762ba
Hamna R <hamnaraeel@gmail.com>
2022-12-07 19:00:04 +0500

    Working on keftside

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+const Main  = (props) => {
+    return (
+        <Container> 
+            main
+        </Container>
+    );
+};
+
+const Container = styled.div``;
+
+export default Main;
---
+++ src/components/RightSide.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+const RightSide  = (props) => {
+    return (
+        <Container> 
+            rightside\
+            
+        </Container>
+    );
+};
+
+const Container = styled.div``;
+
+export default RightSide;
