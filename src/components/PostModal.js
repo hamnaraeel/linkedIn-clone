27a5c1cd0b4d50fa3bfd80a586b5774212a58f48
Hamna R <hamnaraeel@gmail.com>
2022-12-08 23:54:48 +0500

    post modal usestate done

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,7 +1,58 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const PostModal = (props) => {
-  return <Container>post</Container>;
+  const [editorText, setEditorText] = useState("");
+  const reset = (e) => {
+    setEditorText("");
+    props.handleClick(e);
+  };
+  return (
+    <>
+      {props.showModal === "open" && (
+        <Container>
+          <Content>
+            <Header>
+              <h2>Create a Post</h2>
+              <button onClick={(event) => reset(event)}>
+                <img src="/images/close-icon.svg" alt="" />
+              </button>
+            </Header>
+            <SharedContent>
+              <UserInfo>
+                <img src="/images/user.svg" />
+                <span>Name</span>
+              </UserInfo>
+              <Editor>
+                <textarea
+                  value={editorText}
+                  onChange={(e) => setEditorText(e.target.value)}
+                  placeholder="What do you want to talk about?"
+                  autoFocus={true}
+                ></textarea>
+              </Editor>
+            </SharedContent>
+            <SharedCreations>
+              <AttachAssets>
+                <AssetButton>
+                  <img src="/images/photo-icon.svg" />
+                </AssetButton>
+                <AssetButton>
+                  <img src="/images/video-icon.svg" />
+                </AssetButton>
+              </AttachAssets>
+              <SharedComment>
+                <AssetButton>
+                  <img src="/images/comment-icon.svg" />
+                </AssetButton>
+              </SharedComment>
+              <PostButton>post</PostButton>
+            </SharedCreations>
+          </Content>
+        </Container>
+      )}
+    </>
+  );
 };
 
 const Container = styled.div`
@@ -13,6 +64,123 @@ const Container = styled.div`
   z-index: 9999;
   color: white;
   background-color: rgba(0, 0, 0, 0.8);
+  animation: fadeIn 0.3s;
 `;
-
+const Content = styled.div`
+  width: 100%;
+  max-width: 552px;
+  background-color: white;
+  overflow: initial;
+  max-height: 90%;
+  border-radius: 5px;
+  position: relative;
+  flex-direction: column;
+  top: 32px;
+  margin: 0 auto;
+`;
+const Header = styled.div`
+  display: block;
+  padding: 16px 20px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    height: 40px;
+    width: 40px;
+    min-width: auto;
+    color: rgba(0, 0, 0, 0.15);
+    svg,
+    img {
+      pointer-events: none;
+    }
+  }
+`;
+const SharedContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+  vertical-align: baseline;
+  background: transparent;
+  padding: 8px 12px;
+`;
+const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 24px;
+  svg,
+  img {
+    width: 48px;
+    height: 48px;
+    background-clip: content-box;
+    border: 2px solid transparent;
+    border-radius: 50%;
+  }
+  span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    margin-left: 5px;
+  }
+`;
+const SharedCreations = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 24px 12px 16px;
+`;
+const AssetButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  min-width: auto;
+  color: rgba(0, 0, 0, 0.5);
+`;
+const AttachAssets = styled.div`
+  align-items: center;
+  display: flex;
+  padding-right: 0px;
+  ${AssetButton} {
+    width: 40px;
+  }
+`;
+const SharedComment = styled.div`
+  display: flex;
+  margin-right: auto;
+  border-left: 1px solid rgba(0, 0, 0, 0.15);
+  ${AssetButton} {
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+const PostButton = styled.button`
+  min-width: 60px;
+  border-radius: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: #004182;
+  color: #fff;
+  &:hover {
+    background: #004182;
+  }
+`;
+const Editor = styled.div`
+  padding: 12px 24px;
+  textarea {
+    width: 100%;
+    min-height: 100px;
+    resize: none;
+  }
+  input {
+    width: 100%;
+    height: 35px;
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+`;
 export default PostModal;
