4f12ae5b2ba856ea0d4b2c6e38edcb41a91e6e4e
Hamna R <hamnaraeel@gmail.com>
2022-12-08 17:15:06 +0500

    checked till here, no error, store added

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-iQ9pGuwcoYjq_6ScezTBupvYB6x2vT0",
+  authDomain: "linkedin-clone-39b0f.firebaseapp.com",
+  projectId: "linkedin-clone-39b0f",
+  storageBucket: "linkedin-clone-39b0f.appspot.com",
+  messagingSenderId: "296286312347",
+  appId: "1:296286312347:web:485606254d1ccd3194b33e",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
